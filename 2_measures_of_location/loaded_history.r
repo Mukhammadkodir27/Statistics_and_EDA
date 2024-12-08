ec57de660e0e8d4a533560a05b5133423c97de91
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-12-08 19:42:07 +0100

    Add files via upload

---
+++ 2_measures_of_location/loaded_history.r
@@ -0,0 +1,73 @@
+install.packages("cluster")
+install.packages("factoextra")
+install.packages("flexclust")
+install.packages("fpc")
+install.packages("clustertend")
+install.packages("ClusterR")
+library(cluster)
+library(factoextra)
+library(flexclust)
+library(fpc)
+library(clustertend)
+library(ClusterR)
+#Set English in R
+Sys.setenv(LANG = "en")
+ls
+setwd("C:\\Users\\sergiocarcamo\\Library\\CloudStorage\\GoogleDrive-s.carcamojar@student.uw.edu.pl\\Courses\\Statistics and EDA\\Statistics and Explanatory Data Analysis\\2") #Desktop
+setwd("Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/#2") #Desktop
+setwd("Users/sergiocarcamo/Library/CloudStorage") #Desktop
+setwd("/Users/sergiocarcamo/Library/") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/#2") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/2") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/2") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/2") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/#2") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses/Statistics and EDA/")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Courses")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/#2") #Desktop
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/Statistics and Explanatory Data Analysis/")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/Statistics and Exploratory Data Analysis/")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/Statistics and Exploratory Data Analysis/2")
+setwd("/Users/sergiocarcamo/Library/CloudStorage/GoogleDrive-s.carcamojar@student.uw.edu.pl/Mi Unidad/Courses/Statistics and EDA/Statistics and Exploratory Data Analysis/#2")
+apple<-read.csv("Apple_data.csv",
+                header=T,
+                sep=",",
+                dec="." )
+str(apple)
+View(ap)
+View(apple)
+apple$change <- (apple$Close-apple$Open)/apple$Open
+apple$rate.change <- 1+apple$change
+mean(apple$rate.change)
+teens <- read.csv("snsdata.csv")
+teens <- read.csv("snsdata.csv")
+teens <- read.csv("snsdata.csv")
+teens <- read.csv("snsdata.csv")
+teens <- read.csv("snsdata.csv")
+ls
+teens <- read.csv("snsdata.csv")
+# inspect the dataset
+str(teens)
+install.packages("factoextra")
+install.packages("flexclust")
+install.packages("fpc")
+install.packages("clustertend")
+install.packages("cluster")
+install.packages("ClusterR")
+install.packages("flexclust")
+install.packages("factoextra")
+install.packages("factoextra")
+install.packages("flexclust")
+install.packages("fpc")
+install.packages("clustertend")
+install.packages("cluster")
+install.packages("ClusterR")
